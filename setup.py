3d3b05c1a3de72d35c5ae63bd362d022b5ffe4f0
Jeferson P.A <jpafly@gmail.com>
2021-11-17 22:15:35 -0300

    update

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from setuptools import setup, find_packages
+
 setup(
     name='lotusdb',
-    version='0.0.9',
+    version='0.1.1',
     author='d3tu',
     author_email='jpafly@gmail.com',
     description='pydb',
